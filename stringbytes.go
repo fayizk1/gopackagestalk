a9fb0fd9b3949110edecd9827cf3d301ce99c4cf
Fayiz Musthafa <fayizk1@gmail.com>
2017-01-19 16:05:10 +0530

    added json output

--- stringbytes.go
+++ stringbytes.go
@@ -1,27 +1,14 @@
 // The standard library's `strings` package provides many
 // useful string-related functions. Here are some examples
 // to give you a sense of the package.
-
+//S1 OMIT
 package main
 
 import  ("strings"
 	"fmt"
 	"bytes"
 )
-
-// We alias `fmt.Println` to a shorter name as we'll use
-// it a lot below.
-
-
 func main() {
-
-	// Here's a sample of the functions available in
-	// `strings`. Since these are functions from the
-	// package, not methods on the string object itself,
-	// we need pass the string in question as the first
-	// argument to the function. You can find more
-	// functions in the [`strings`](http://golang.org/pkg/strings/)
-	// package docs.
 	fmt.Println("Contains:  ", strings.Contains("test", "es"))
 	fmt.Println("Count:     ", strings.Count("test", "t"))
 	fmt.Println("HasPrefix: ", strings.HasPrefix("test", "te"))
@@ -36,11 +23,7 @@ func main() {
 	fmt.Println("ToUpper:   ", strings.ToUpper("test"))
 	fmt.Println()
 
-	// Not part of `strings`, but worth mentioning here, are
-	// the mechanisms for getting the length of a string in
-	// bytes and getting a byte by index.
-
-
+	//S2 OMIT
 	fmt.Println("Contains:  ", bytes.Contains([]byte("test"), []byte("es")))
 	fmt.Println("Count:     ", bytes.Count([]byte("test"), []byte("t")))
 	fmt.Println("HasPrefix: ", bytes.HasPrefix([]byte("test"), []byte("te")))
@@ -51,12 +34,8 @@ func main() {
 	fmt.Println("Split:     ", bytes.Split([]byte("a-b-c-d-e"), []byte("-")))
 	fmt.Println("ToLower:   ", string(bytes.ToLower([]byte("TEST"))))
 	fmt.Println("ToUpper:   ", string(bytes.ToUpper([]byte("test"))))
-
-
-
-	
 }
-
+//S3 OMIT
 // Note that `len` and indexing above work at the byte level.
 // Go uses UTF-8 encoded strings, so this is often useful
 // as-is. If you're working with potentially multi-byte
